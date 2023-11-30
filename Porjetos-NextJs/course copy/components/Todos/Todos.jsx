f5295e0713e5a7decc7ccab254b77b83e109f996
GagoBraboH2oh11 <gabriel.dev.prog@gmail.com>
2023-11-30 11:06:54 -0300

    new

--- Porjetos-NextJs/course copy/components/Todos/Todos.jsx
+++ Porjetos-NextJs/course copy/components/Todos/Todos.jsx
@@ -1,23 +1,22 @@
-export async function getStaticsProps(){
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos')
-  const todos = await data.json()
- 
-  return{
-    props:{todos},
-  }
+const GetData = async()=>{
+  const data= await fetch('https://jsonplaceholder.typicode.com/todos')
+  return data.json()
+  
 }
 
-function Todos({todos}) {
+async function Todos() {
+  const data = await GetData()
   return (
-    <> 
-    <h1>Lista de tarefas</h1>
+    <>
+    <h1>Lista de Afazeres</h1>
     <ul>
-          {todos.map((todo)=>(
-            <li key={todo.id}>{todo.title}</li>
-          ))}
+      {data.map((todo)=>(
+        <li key={todo.id}>{todo.id}-{todo.title}</li>
+      ))}
     </ul>
-  </>
-  )
+    </>
+  );
+
   
 }
 
